bfb8f5e8b8c6b0003804c7c8a708042e5ebb1c71
ricaricabile <gabrielmamon288@gmail.com>
2024-06-15 01:47:58 +0800

    initial commit

--- client/src/redux/app/app.reducer.js
+++ client/src/redux/app/app.reducer.js
@@ -1,19 +1,9 @@
 const INITIAL_STATE = {
-    weight: 5,
-    result: 0,
-    incm: 0,
-    left: 1,
-    right: 1,
-    resultKegs: 0
+    articles: [],
 };
 const appReducer = (state = INITIAL_STATE, action) => {
     switch(action.type){
-        case 'SET_WEIGHT': return {...state, weight: action.payload};
-        case 'SET_HEIGHT': return {...state, incm: action.payload};
-        case 'SET_LEFT': return {...state, left: action.payload};
-        case 'SET_RIGHT': return {...state, right: action.payload};
-        case 'SET_RESULT_POUNDS': return {...state, result: action.payload};
-        case 'SET_RESULT_KGS': return {...state, resultKegs: action.payload};
+        case 'SET_ARTICLES': return {...state, articles: action.payload};
         default: 
             return state;
     }
--- server.js
+++ server.js
@@ -1,7 +1,9 @@
+
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+const axios = require('axios');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,7 +11,7 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
-
+if(process.env.NODE_ENV != 'production') require('dotenv').config();
 if(process.env.NODE_ENV == 'production'){
     app.use(express.static(path.join(__dirname, 'client/build')));
 }
@@ -18,32 +20,8 @@ app.listen(port, error => {
     if(error) throw error;
     console.log('Server running on port '+ port);
 });
-app.post('/bmicalc', (req, res) =>{
-    let weight = parseFloat(req.body.weight);
-    let InchesCentimeters = parseFloat(req.body.height);
-    let height = 0;
-
-    if(req.body.unitW == 'Ibs'){
-        weight = parseFloat(weight * 0.453592); 
-    }
-    if(req.body.unitH == 'in'){
-        InchesCentimeters = InchesCentimeters * 0.0254;
-        height = InchesCentimeters * InchesCentimeters;
-    }
-    if(req.body.unitH == 'cm'){
-        InchesCentimeters = InchesCentimeters * 0.01;
-        height = InchesCentimeters * InchesCentimeters;
-    }
-    if(req.body.unitH == 'ft'){
-        let Foot = req.body.height;
-        let remainder = req.body.heightRemainder;
-        let others = parseFloat(remainder / 12);
-        let hold = Foot + others;
-        height = hold * 0.3048;
-        height = height * height;
-    }
-    const result = parseFloat(weight / height);
-    
-    res.status(200).send({result: result.toFixed(1)});
+app.get('/fetchnewsorgdata', async (req, res) => {
+    const result = await axios.get(`https://newsapi.org/v2/everything?q=boxing&apiKey=${process.env.NEWS_API_ORG_KEY}`);
+    res.status(200).send(result.data);
 });
 
